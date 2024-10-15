96885eb54e688f8b1292883f496af9dca706bbb1
KevinMisati <kevinmosigisi2001@gmail.com>
2024-10-15 05:22:12 +0300

    test git credentials

--- src/components/Account/Logout.js
+++ src/components/Account/Logout.js
@@ -1,7 +1,6 @@
 import React,{useContext,useState,useEffect} from 'react'
 import classes from "./Logout.module.css"
 import { useNavigate } from "react-router-dom"
-import { getAuth, signOut } from "firebase/auth";
 import { CartContext } from '../../Context'
 
 
@@ -12,48 +11,23 @@ const Logout = () => {
     const{logOutUser} = useContext(CartContext)
     const navigate = useNavigate()
 
-    //const {user} = useContext(CartContext)
-
-    const auth = getAuth();
     const handleLogOut = (e) => {
         e.preventDefault()
-        signOut(auth).then(() => {
-            navigate("/")
-            console.log("success")
-            logOutUser()
-        }).catch((error) => {
-            console.log(error)
-        });
-    }
-useEffect(() => {
-    const user = auth.currentUser;
-        if (user !== null) {
-        const displayName = user.displayName;
-        const email = user.email;
-        const photoURL = user.photoURL;
-        const emailVerified = user.emailVerified;
-        console.log(email)
-        setUserDetails({
-            email:email
-        })
-        }
-
-},
-[])
-        
+    
+    }   
     
     return (
         <div className={classes['logout-container']}>
             <div className={classes["logout"]}>
                 <h1>My Account</h1>
-                <div className={classes["logout-btn"]}>
-                    <button onClick={handleLogOut}>log out</button>
-                </div>
                 <div>
                     <h3>account details</h3>
                     <div>
                         <p>Email: {userDetails.email}</p>
                     </div>
+                </div>
+                <div className={classes["logout-btn"]}>
+                    <button onClick={handleLogOut}>log out</button>
                 </div>
             </div>
         </div>
