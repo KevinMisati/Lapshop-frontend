fc2789dbcbeb4ccebfd418887bdcf74166ffe915
KevinMisati <kevinmosigisi2001@gmail.com>
2025-01-02 12:40:44 +0300

    added loaders

--- src/Pages/ProductInfo.js
+++ src/Pages/ProductInfo.js
@@ -4,10 +4,13 @@ import {useParams} from "react-router-dom"
 import AddToCartButton from '../components/Utilities/AddToCartButton'
 import { CartContext } from '../Context'
 import { apiService } from '../axios'
+import Loader from '../components/Loader'
 
 const ProductInfo = () => {
     const [product,setProduct] = useState({})
     const {id} = useParams("id")
+
+    const [isLoading,setIsLoading] = useState(true)
 
     const {add_to_cart} = useContext(CartContext)
     const handleItemIncrement = (id) => {
@@ -18,33 +21,45 @@ const ProductInfo = () => {
         apiService({
             url:`laptops/${id}`,
             method:"GET"
-        }).then(res => setProduct(res.data))
-        .catch(error => console.log(error)) 
+        }).then(res => {
+            setProduct(res.data)
+            setIsLoading(false)
+        })
+        .catch(error => {
+            console.log(error)
+            setIsLoading(false)
+        }) 
     }, [id]) 
 
     
     
     const {title,image,info} = product
     return (
-        <div className={classes["product-info-container"]}>
-        <div className={classes["product-info"]}>
-            <div className={classes["img-container"]}>
-                <img alt={product.model_name} src={product.image} />
-            </div>
-            <div className={classes["product-desc"]}>
-                <h2>{product.model_name}</h2>
-                <p className={classes.info}>{product.description}</p>
-                <p className={classes.info}>$ {product.price}</p>
-                <div className={classes["add-btn"]} onClick={handleItemIncrement}>
-                    <AddToCartButton
-                        color="#555"
-                        font="1.05rem"
-                        text="add to cart" />
-                </div>
+        
+            <div className={classes["product-info-container"]}>
+                {!isLoading ?
+                    <div className={classes["product-info"]}>
+                        <div className={classes["img-container"]}>
+                            <img alt={product.model_name} src={product.image} />
+                        </div>
+                        <div className={classes["product-desc"]}>
+                            <h2>{product.model_name}</h2>
+                            <p className={classes.info}>{product.description}</p>
+                            <p className={classes.info}>$ {product.price}</p>
+                            <div className={classes["add-btn"]} onClick={handleItemIncrement}>
+                                <AddToCartButton
+                                    color="#555"
+                                    font="1.05rem"
+                                    text="add to cart" />
+                            </div>
+                            
+                        </div>
+                    </div> :
                 
-            </div>
-        </div>
-        </div>
+                <Loader />
+                }
+            </div> 
+        
     )
 }
 
