24bd14857e4b42f8894d82f4dedb1c1050500200
kevinmm001 <kevinm@teknobuilt.com>
2025-05-28 06:04:09 +0300

    reduced the products images widths

--- src/components/Utilities/SingleProduct.js
+++ src/components/Utilities/SingleProduct.js
@@ -1,4 +1,4 @@
-import React,{useContext,useState} from 'react'
+import React,{useContext} from 'react'
 import classes from "./SingleProduct.module.css"
 import Button from './Button'
 import { Link} from 'react-router-dom'
