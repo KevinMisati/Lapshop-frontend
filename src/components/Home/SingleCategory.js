9352cad5f20ab5c0bb8149c35eb277d08e70ff0b
kevinmm001 <kevinm@teknobuilt.com>
2025-05-29 10:38:20 +0300

    added the similar products section

--- src/components/Home/SingleCategory.js
+++ src/components/Home/SingleCategory.js
@@ -1,4 +1,5 @@
-import classes from "./SingleCategory.module.css"
+//import classes from "./SingleCategory.module.css"
+import classes from "../Utilities/SingleProduct.module.css"
 import { useNavigate } from 'react-router-dom'
 
 const SingleCategory = ({category}) => {
