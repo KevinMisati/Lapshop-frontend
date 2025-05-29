d0c801685d1e3d0b7cc13917574b9165b487ea60
kevinmm001 <kevinm@teknobuilt.com>
2025-05-29 10:13:29 +0300

    added the similar products section

--- src/components/Utilities/helpers.js
+++ src/components/Utilities/helpers.js
@@ -1,6 +1,7 @@
 export const getSliderSettings = (width) => {
     let slides = 1
-    if(width < 500) slides = 1
+    if(width < 300) slides = 1
+    if(width < 500) slides = 2
     else if(width < 600) slides = 2
     else if(width < 700) slides = 3
     else if(width < 1000) slides = 3
